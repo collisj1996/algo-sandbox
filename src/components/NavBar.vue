f3bf5fa1cc49c0cbcd2becf12f46810070829d20
collisj1996 <jackcollis96@gmail.com>
2022-07-07 14:14:24 +0100

    Add nav bar logo

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -10,17 +10,31 @@ const navItems: NavItem[] = [
 </script>
 
 <template>
-  <div id="navbar-container">
-    <router-link class="navbar-item" v-for="(navItem, index) in navItems" :key="index" :to="navItem.route">{{ navItem.name }}</router-link>
+  <div id="navbar-outer">
+    <img id="navbar-logo" src="./icons/logo-white.png" alt="logo" id="navbar-logo" />
+    <div id="navbar-container">
+      <router-link class="navbar-item" v-for="(navItem, index) in navItems" :key="index" :to="navItem.route">{{ navItem.name }}</router-link>
+    </div>
   </div>
 </template>
 
 <style scoped>
+#navbar-outer {
+  display: flex;
+  margin-top: 20px;
+}
+
 #navbar-container {
   height: 50px;
   display: flex;
   justify-content: left;
   align-items: center;
+}
+
+#navbar-logo {
+  height: 50px;
+  width: 50px;
+  margin-right: 10px;
 }
 
 .navbar-item {
