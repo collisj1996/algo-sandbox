28ababa6910a3c7b2b6f28e7a8d460ea193184d2
collisj1996 <jackcollis96@gmail.com>
2022-07-07 14:06:10 +0100

    + Generate cells grid + Draw debug cell info to canvas

--- src/components/PathFinding/PathFindingGrid.vue
+++ src/components/PathFinding/PathFindingGrid.vue
@@ -11,8 +11,9 @@ export default defineComponent({
     const state = reactive({
       vueCanvas: null as HTMLCanvasElement | HTMLElement | null,
       vueContext: null as CanvasRenderingContext2D | null,
-      vueCanvasWidth: 500 as number,
-      vueCanvasHeight: 500 as number,
+      vueCanvasWidth: 1240 as number,
+      vueCanvasHeight: 680 as number,
+      canvasPadding: 20,
       cellGrid: null as Cell[][] | null,
       gridStep: 10 as number,
     });
@@ -21,6 +22,8 @@ export default defineComponent({
   },
   mounted() {
     this.initialiseCanvas();
+    this.generateCellGrid();
+    this.drawCells();
   },
   methods: {
     changeVueCanvas(canvas: HTMLCanvasElement | HTMLElement | null) {
@@ -36,10 +39,30 @@ export default defineComponent({
       const ctx = canvas.getContext("2d");  
       this.changeVueCanvas(canvas);
       this.changeVueConext(ctx);
+      this.drawCanvasBackground();
       this.updateCanvas();
+      this.generateCellGrid();
+      this.drawCells();
     },
     generateCellGrid() {
-      console.log('test');
+      const cellGrid: Cell[][] = Array.from(Array(this.gridStep), () => Array(this.gridStep));
+
+      for(let i=0; i<this.gridStep; i++) {
+        for(let j=0; j<this.gridStep; j++) {
+          const xCoord = this.vueCanvasWidth / this.gridStep * i;
+          const yCoord = this.vueCanvasHeight / this.gridStep * j;
+
+          cellGrid[i][j] = {
+            x: xCoord,
+            y: yCoord,
+            start: false,
+            end: false,
+            wall: false,
+          }
+        }
+      }
+
+      this.cellGrid = cellGrid;
     },
     drawGridLines() {
       const cellWidth = this.vueCanvasWidth / this.gridStep;
@@ -61,15 +84,30 @@ export default defineComponent({
 
     },
     drawCells() {
-      console.log('test');
+      const cellWidth = this.vueCanvasWidth / this.gridStep;
+      const cellHeight = this.vueCanvasHeight / this.gridStep;
+      
+      for(let i=0; i<this.gridStep; i++) {
+        for(let j=0; j<this.gridStep; j++) {
+          const currentCell = this.cellGrid[i][j];
+          this.vueContext.font = "1rem Arial";
+          this.vueContext.fillStyle = "red"
+          this.vueContext.fillText(`${currentCell.x}x,${currentCell.y}y`, (currentCell.x + cellWidth / 2) - 40, (currentCell.y + cellHeight / 2));
+        }
+      }
     },
     updateCanvas() {
-      this.vueContext.clearRect(0, 0, this.vueCanvasWidth, this.vueCanvasHeight);
+      this.clearCanvasBackground();
+      this.drawCanvasBackground();
+      this.drawGridLines();
+    },
+    drawCanvasBackground() {
       this.vueContext.fillStyle = "white";
       this.vueContext.fillRect(0, 0, this.vueCanvasWidth, this.vueCanvasHeight);
-      this.drawGridLines();
-      this.drawCells();
     },
+    clearCanvasBackground() {
+      this.vueContext.clearRect(0, 0, this.vueCanvasWidth, this.vueCanvasHeight);
+    }
   }
 })
 </script>
@@ -78,13 +116,15 @@ export default defineComponent({
   <div id="canvas-container">
     <canvas id="grid-canvas" :width="vueCanvasWidth" :height="vueCanvasHeight"></canvas>
     <div id="settings-container">
-      Canvas Height
-      <input v-model="vueCanvasHeight" @input="updateCanvas" />
-      Canvas Width
-      <input v-model="vueCanvasWidth" @input="updateCanvas" />
       Grid Step
       <input v-model="gridStep" @input="updateCanvas" />
     </div>
   </div>
 </template>
+
+<style scoped>
+#grid-canvas {
+  padding: 20px;
+}
+</style>
  
